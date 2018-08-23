0cc0e2347a4b8004b3e45b541b6b832a47b777ca
sebastien michea <smichea@gmail.com>
2018-08-23 20:57:35 +0200

    serve site under /pos/

--- pos/service-worker.js
+++ pos/service-worker.js
@@ -1,54 +1,54 @@
 var cacheName="pwa-pos_0.16.17";
-var filesToCache=[];
-var filesToCache2 = [
-    '/',
-    '/index.html',
-    '/pos.js',
-    '/catalog.html',
-    '/catalog.js',
-    '/newPurchase.html',
-    '/newPurchase.js',
-    '/orders.html',
-    '/orders.js',
-    '/productCategories.html',
-    '/productCategories.js',
-    '/purchases.html',
-    '/purchases.js',
-    '/lib/DataStore.js',
-    '/lib/InventoryStore.js',
-    '/lib/JsBarcode.min.js',
-    '/lib/OrderStore.js',
-    '/lib/papaparse.min.js',
-    '/lib/ProductCategoryStore.js',
-    '/lib/ProductStore.js',
-    '/lib/PurchaseStore.js',
-    '/lib/vanilla-datatables.js',
-    '/lib/xls-export.js',
-    '/css/bootstrap.min.css',
-    '/css/pricetags.css',
-    '/css/receipt.css',
-    '/img/.01coin.jpg',
-    '/img/.05coin.jpg',
-    '/img/.25coin.jpg',
-    '/img/10coin.jpg',
-    '/img/20php.jpg',
-    '/img/50php.jpeg',
-    '/img/100php.jpg',
-    '/img/200php.jpeg',
-    '/img/500php.jpeg',
-    '/img/1000php.jpg',
-    '/img/logo.png',
-    '/img/1000php.jpg'
+//var filesToCache=[];
+var filesToCache = [
+    '/pos/',
+    '/pos/index.html',
+    '/pos/pos.js',
+    '/pos/catalog.html',
+    '/pos/catalog.js',
+    '/pos/newPurchase.html',
+    '/pos/newPurchase.js',
+    '/pos/orders.html',
+    '/pos/orders.js',
+    '/pos/productCategories.html',
+    '/pos/productCategories.js',
+    '/pos/purchases.html',
+    '/pos/purchases.js',
+    '/pos/lib/DataStore.js',
+    '/pos/lib/InventoryStore.js',
+    '/pos/lib/JsBarcode.min.js',
+    '/pos/lib/OrderStore.js',
+    '/pos/lib/papaparse.min.js',
+    '/pos/lib/ProductCategoryStore.js',
+    '/pos/lib/ProductStore.js',
+    '/pos/lib/PurchaseStore.js',
+    '/pos/lib/vanilla-datatables.js',
+    '/pos/lib/xls-export.js',
+    '/pos/css/bootstrap.min.css',
+    '/pos/css/pricetags.css',
+    '/pos/css/receipt.css',
+    '/pos/img/.01coin.jpg',
+    '/pos/img/.05coin.jpg',
+    '/pos/img/.25coin.jpg',
+    '/pos/img/10coin.jpg',
+    '/pos/img/20php.jpg',
+    '/pos/img/50php.jpeg',
+    '/pos/img/100php.jpg',
+    '/pos/img/200php.jpeg',
+    '/pos/img/500php.jpeg',
+    '/pos/img/1000php.jpg',
+    '/pos/img/logo.png',
+    '/pos/img/1000php.jpg'
   ];
 
   self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
-    //e.waitUntil(
+    e.waitUntil(
       caches.open(cacheName).then(function(cache) {
         console.log('[ServiceWorker] Caching '+filesToCache.length+' files to cache='+cacheName);
         return cache.addAll(filesToCache);
       })
-    //);
+    );
   });
 
   self.addEventListener('activate', function(e) {
